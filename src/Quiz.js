b1d5b4d891ddfb0aa38199062cbafa116438cfca
Atanu Sarkar <atsar@microsoft.com>
2020-04-14 00:53:30 +0530

    3.2

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,7 +13,7 @@ class Quiz extends Component {
   render() {
     return (
       <div>
-        <QuizQuestion quiz_question={quizData.quiz_questions[quiz_position-1]}/>
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
       </div>
     );
   }
